7ce2e5acc0cda87bd324559b2b72810ec2b3f6a9
Marc De Jesus <marcantoniodejesus@yahoo.com>
2025-02-21 11:53:25 -0500

    background music

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import './background.mp3';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <audio src = "./background.mp3" autoPlay loop/>
       </header>
     </div>
   );
