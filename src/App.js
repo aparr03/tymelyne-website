1a73437b36c7171d99a0f26278d90ed355597510
Marc De Jesus <131229911+marcdejesus@users.noreply.github.com>
2025-02-24 17:31:51 +0000

    404

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import NoPage from "./pages/NoPage";
+import CssBaseline from '@mui/material/CssBaseline';
 
 export default function App(props) {
   return (
@@ -12,6 +14,7 @@ export default function App(props) {
         <Route path="/home" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="*" element={<NoPage />} />
       </Routes>
     </BrowserRouter>
   );
